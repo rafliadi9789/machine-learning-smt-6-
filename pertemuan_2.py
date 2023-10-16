25c862e067c033fe0cd5240bc66b9758096a91a8
Rafli Adiputra <145460839+rafliadi9789@users.noreply.github.com>
2023-10-16 18:54:56 +0700

    Tugas sesi 2 Machine Learning

---
+++ pertemuan_2.py
@@ -0,0 +1,41 @@
+
+from numpy import *
+import numpy as np
+tema1 = "''''''''' JAWABAN SOAL NOMOR 1 ''''''''''''''"
+nama = "Rafli Adiputra"
+nim_mhs = "20200801417"
+mata_kuliah = "Machine Learning"
+tugas_matkul = "Pertemuan 2!"
+
+print(tema1)
+print("DATA DIRI")
+print("Nama : ", nama)
+print("NIM: ", nim_mhs)
+print("Perkuliahan: ", mata_kuliah)
+print("Tugas : ", tugas_matkul)
+
+tema2 = "''''''''' JAWABAN SOAL NOMOR 2 ''''''''''''''"
+matriksC = [[1, 2, 3], [4, 5, 6]]
+
+print(tema2)
+print(matriksC)
+
+tema3 = "''''''''' JAWABAN SOAL NOMOR 3 ''''''''''''''"
+matriks3 = [[2, 2], [2, 2], [2, 2]]
+
+print(tema3)
+print(matriks3)
+
+
+tema5 = "''''''''' JAWABAN SOAL NOMOR 5 ''''''''''''''"
+matriks5 = range(32)
+matriks5 = reshape(matriks5, (4, 8))
+
+print(tema5)
+print(matriks5)
+
+tema6 = "''''''''' JAWABAN SOAL NOMOR 6 ''''''''''''''"
+
+matriks6 = np.random.randint(1, 5, (3, 30))
+print(tema6)
+print(matriks6)
